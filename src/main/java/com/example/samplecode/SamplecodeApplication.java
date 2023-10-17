75089683407dc477d590b1a8695dce948bdaa4cb
lakshmimarar <lakshmimarar77@gmail.com>
2023-10-17 09:20:07 +0000

    Init the project

---
+++ src/main/java/com/example/samplecode/SamplecodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplecode/SamplecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
